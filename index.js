444b15b08a4832b6afc3d416537f8c9ab0397594
Matthew Dahl <sportinmd@me.com>
2022-07-09 15:59:01 -0700

    commit tree 08a6b20fa71bd2061b899efc86240e6913ea8930

---
+++ index.js
@@ -0,0 +1,18 @@
+function shout(string) {
+    //todo
+}
+function shout(string) {
+    return string;
+}
+function shout(string) {
+    return string.toUpperCase();
+}
+function whisper(string) {
+    //todo
+}
+function whisper(string) {
+    return whisper;
+}
+function whisper(string) {
+    return string.toLowerCase()
+}
